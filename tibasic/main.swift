dfc2a1bb770a8acb59e36ee9683eccec21b4fb37
Michael Welch <michaelgwelch@gmail.com>
2015-07-18 17:22:12 -0500

    Initial Commit

---
+++ tibasic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tibasic
+//
+//  Created by Michael Welch on 7/18/15.
+//  Copyright © 2015 Michael Welch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
