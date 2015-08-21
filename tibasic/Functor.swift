9111279debd949efa5718ebe6d9d0074ed128d16
Michael Welch <michaelgwelch@gmail.com>
2015-08-21 09:57:49 -0500

    Add fmap operator for List<T> and [T]

--- tibasic/Functor.swift
+++ tibasic/Functor.swift
@@ -21,8 +21,23 @@ public func <§><A,B>(lhs:A->B, rhs:A?) -> B? {
     return rhs.map(lhs)
 }
 
+public func <§><A,B>(lhs:A->B, rhs:[A]) -> [B] {
+    return rhs.map(lhs)
+}
+
 
 // Tuple Functor on rhs
 public func <§><A,B,C>(lhs:A->B, rhs:(A,C)) -> (B,C) {
     return (lhs(rhs.0), rhs.1)
+}
+
+public func <§><A,B>(lhs:A->B, rhs:List<A>) -> List<B> {
+    return (rhs.map(lhs))
+}
+
+
+extension List {
+    public func map<B>(f:T->B) -> List<B> {
+        return self.reduce(List<B>.Nil, combine: {List<B>.Cons(h: f($1), t: $0)})
+    }
 }
